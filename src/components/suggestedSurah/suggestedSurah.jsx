b1a8fc120a1290a6a13d3fbf6544f4f798c6e9ca
Rashed Abdullah <maarashed40@gmail.com>
2024-02-13 20:34:40 +0600

    react.js

--- src/components/suggestedSurah/suggestedSurah.jsx
+++ src/components/suggestedSurah/suggestedSurah.jsx
@@ -2,7 +2,7 @@ const SuggestedSurah = () => {
   return (
     <div>
       <div className="p-5">
-        <ul className="flex align-middle justify-center gap-5 overflow-y-auto no-scrollbar p-5">
+        <ul className="flex align-middle lg:justify-center gap-5 overflow-y-auto no-scrollbar p-5">
           <li>
             <a
               className="border flex gap-1 border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
