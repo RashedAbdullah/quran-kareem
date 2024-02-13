b8c51c278682ca4a48140ed1c8e8297dafcddc5f
Rashed Abdullah <maarashed40@gmail.com>
2024-02-13 20:32:27 +0600

    css issue

--- src/components/suggestedSurah/suggestedSurah.jsx
+++ src/components/suggestedSurah/suggestedSurah.jsx
@@ -2,56 +2,56 @@ const SuggestedSurah = () => {
   return (
     <div>
       <div className="p-5">
-        <ul className="flex align-middle justify-center gap-5 flex-wrap">
+        <ul className="flex align-middle justify-center gap-5 overflow-y-auto no-scrollbar p-5">
           <li>
             <a
-              className="border border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
+              className="border flex gap-1 border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
               href=""
             >
-              سُورَةُ آلِ عِمۡرَانَ
+              <p>سُورَةُ</p> <p>البَقَرَةِ</p>
             </a>
           </li>
           <li>
             <a
-              className="border border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
+              className="border flex gap-1 border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
               href=""
             >
-              سُورَةُ البَقَرَةِ
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a
-              className="border border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
-              href=""
-            >
-              سُورَةُ النِّسَاءِ
-            </a>
-          </li>
-          <li>
-            <a
-              className="border  border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
-              href=""
-            >
-              سُورَةُ آلِ عِمۡرَانَ
+              <p>سُورَةُ</p> <p>البَقَرَةِ</p>
             </a>
           </li>
           <li>
             {" "}
             <a
-              className="border  border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
+              className="border flex gap-1 text-wrap border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
               href=""
             >
-              سُورَةُ البَقَرَةِ
+              <p>سُورَةُ</p> <p>البَقَرَةِ</p>
+            </a>
+          </li>
+          <li>
+            <a
+              className="border flex gap-1 border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
+              href=""
+            >
+              <p>سُورَةُ</p> <p>البَقَرَةِ</p>
             </a>
           </li>
           <li>
             {" "}
             <a
-              className="border border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
+              className="border flex gap-1 border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
               href=""
             >
-              سُورَةُ النِّسَاءِ
+              <p>سُورَةُ</p> <p>البَقَرَةِ</p>
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a
+              className="border flex gap-1 border-green-900 text-[#043535] rounded-full text-lg px-6 py-1 hover:bg-[#043939] hover:text-white transition hover:border-transparent"
+              href=""
+            >
+              <p>سُورَةُ</p> <p>البَقَرَةِ</p>
             </a>
           </li>
         </ul>
